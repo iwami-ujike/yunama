5bada0c3a038717c622fd75bdc1b8d618353498c
torabit <pujigami@gmail.com>
2021-05-04 18:34:41 +0900

    button no scripts

--- yunama2/Assets/Scripts/UIContorller/ControlButtons.cs
+++ yunama2/Assets/Scripts/UIContorller/ControlButtons.cs
@@ -9,12 +9,11 @@ public class ControlButtons : MonoBehaviour
     CursorController cursorController;
     DungeonController dungeonController;
     
-    GameObject chosenObject;
 
     public float waitInput = 0.1f;
     float timer = 0;
     public bool waitingInput = true;
-
+    
     void Start() {
         cursorController = cursor.GetComponent<CursorController>();
         dungeonController = dungeonControllerGO.GetComponent<DungeonController>();
@@ -64,17 +63,12 @@ public class ControlButtons : MonoBehaviour
     public void onClickDown() {
         cursorController.moveDown();
     }
-
+    
     public void clickCircle() {
-        chosenObject = null;
+        cursorController.destroyThisBlock();
+    }
 
-        Ray ray = Camera.main.ScreenPointToRay(cursorController.transform.position);
-        Vector2 position = new Vector2(cursorController.transform.position.x + 0.5f, cursorController.transform.position.y + 0.25f);
-        RaycastHit2D hit2d = Physics2D.Raycast(position, new Vector2(0f,0f));
-
-        if (hit2d != null) {
-            chosenObject = hit2d.transform.gameObject;
-            dungeonController.destroyBlock(chosenObject);
-        }
+    public void clickDelta() {
+        cursorController.cameraZoom();    
     }
 }
